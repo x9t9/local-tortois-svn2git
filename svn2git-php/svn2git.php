d5d02c75c696953727d06b65ad58dbc1637e9cac
x9t9 <k@x9t9.com>
2017-01-03 12:56:41 +0800

    Minor Changes

--- svn2git-php/svn2git.php
+++ svn2git-php/svn2git.php
@@ -1,12 +1,21 @@
+<!DOCTYPE html>
 <?php
 /*
 *
 * A Script to automate migration process for SVN2GIT
 *
-* Requierments : git ( installed into PATH ) , svn server ( svnserve.exe ), RUBY , Gem svn2git ,XAMPP ( or any other *AMP stack )
+* Requierments : git ( installed into PATH ) , svn server ( svnserve.exe ), RUBY , Gem svn2git / 
 *
 */
-
+?>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>SVN2GIT</title>
+</head>
+<body>
+<?php
 error_reporting(E_ALL); 
 ini_set('max_execution_time', 300); //300 seconds = 5 minutes
 set_time_limit(300); // Just to be sure ..
@@ -30,8 +39,8 @@ set_time_limit(300); // Just to be sure ..
 *
 */
 
-$dirs = array_filter(glob('*'), 'is_dir');
-print_r( $dirs);
+$SVNserver = "127.0.0.1";
+$SVNprotocol = "svn://"; // svn:// , https://, file\\\, 
 $SVNroot = "D:/Repositories"; // no trailing slash
 $GITroot = "D:/MIG";
 $SVNusername = "Magic-OFFICE"; // SVN username to access SVN repos .
@@ -58,13 +67,20 @@ $usersMap = array (
 *
 */
 
+// $dirs = array_filter(glob('*'), 'is_dir');
+// print_r( $dirs);
+
+
 $directories = glob($SVNroot  . '/*' , GLOB_ONLYDIR);
 
 // print_r( $directories); // Debug 
 
+echo nl2br('Starting process for '.count($directories).' directories.');
+
 foreach ( $directories as $repo ) {
 	$repoName = str_replace($SVNroot.'/' , '' , $repo );
-	echo ' </br>Starting migration on '. $repoName . PHP_EOL . '</br>';
+	
+	echo ' <h2>Starting migration on '. $repoName . PHP_EOL . '</h2>';
 	
 	// Replace unwanted strings , Surfix , Prefix 
 	if ( isset($replace_in_name) || !empty($replace_in_name)) {
@@ -90,16 +106,17 @@ foreach ( $directories as $repo ) {
 	
 	if (!file_exists($GITrepoPath)) {
 			mkdir($GITrepoPath, 0755, true);
-			echo $GITrepoPath . ' CREATED .. ' . PHP_EOL . "<br>";
+			echo $GITrepoPath . nl2br('folder was created ..\n ') ;
 
 			// Create authors.txt file requiered for migration ...
 			$myfile = fopen($GITrepoPath."/authors.txt", "a") or die("Unable to open file!");
 			$txt = "";
 			foreach ( $usersMap as $SVNuser => $GITuser   ) {
-			$txt .=  $SVNuser . "=" . $GITuser . PHP_EOL . "<br>";
+				$txt .=  $SVNuser . "=" . $GITuser . PHP_EOL . "<br>";
 			}
 			
 			fwrite($myfile, "\n". $txt);
+			echo nl2br( $GITrepoPath . '/ authors.txt was created ..\n ') ;
 			fclose($myfile);
 			
 			// Launch SVN2GIT
@@ -118,10 +135,28 @@ foreach ( $directories as $repo ) {
 			*/
 			
 			// we need to put git into PATH 
-			//http://stackoverflow.com/questions/26620312/installing-git-in-path-with-github-client-for-windows
+			// @see : http://stackoverflow.com/questions/26620312/installing-git-in-path-with-github-client-for-windows
 			
 			// $cmd = "cd c:/Users/Magic-OFFICE/AppData/Local/GitHub/GitHub.appref-ms --open-shell && svn2git svn://192.168.1.10/". $repoName . " --username " .$SVNusername ." --authors authors.txt --notags --nobranches --notrunk";
 			$cmd = "cd ".$GITrepoPath." && svn2git svn://127.0.0.1/". $repoName . " --username " .$SVNusername ." --authors authors.txt --notags --nobranches --notrunk";
+			
+			// @see : http://stackoverflow.com/questions/41420717/cmd-from-php-script-get-feedback/41421196#41421196
+			
+			// $output_storage = [];
+			// $output_showed = [];
+			// $result = null;
+			
+			// exec($cmd, $output_storage, $result);
+			
+			// while( $result === null ){
+				// $diff = array_diff($output_storage, $output_showed);
+				// if( $diff ){
+					// // all new outputs here as $diff
+					// $output_showed = $diff;
+					// echo $output_showed;
+				// }
+			// }
+			
 			$output = shell_exec($cmd);
 			echo "<pre>$output</pre>";
 			echo "<pre>$cmd</pre>";
@@ -129,13 +164,15 @@ foreach ( $directories as $repo ) {
 			// system("cmd /c cd D: cd MIG\\ dir>dir.txt");
 			$cmd2 = $GITrepoPath . '/ ' . 'dir>dir.txt';
 
-			echo '$repoName -- > ' .$repoName . "<br>";
-			echo '$GITrepoPath -- > ' .$GITrepoPath. "<br>";
-			echo '$repoName -- > svn://127.0.0.1/' .$repoName. "<br>";
+			echo '[ var $repoName ] == > ' .$repoName . "<br>";
+			echo '[ var $GITrepoPath ] == > ' .$GITrepoPath. "<br>";
+			echo '[ SVN server , repo location  ] == > ' . $SVNprotocol . $SVNserver. '/'.$repoName. "<br>";
 
 		} else {
-			echo $GITrepoPath . " Already exists ..";
+			echo nl2br("Aborting - <b>"). $GITrepoPath . nl2br("</b> Folder Already exists ..");
 		}
 	}
 
 ?>
+</body>
+</html>
